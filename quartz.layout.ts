ab9d7e4826c92c2c045b5bcee5683831c8d71851
nuancedtire <149303031+nuancedtire@users.noreply.github.com>
2024-08-07 02:53:47 +0000

    yes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,26 +6,22 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer(),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.Breadcrumbs({
+      spacerSymbol: "❯", // symbol between crumbs
+      rootName: "Home", // name of first/root element
+      resolveFrontmatterTitle: true, // whether to resolve folder names through frontmatter titles
+      hideOnRoot: true, // whether to hide breadcrumbs on root `index.md` page
+      showCurrentPage: true, // whether to display the current page in the breadcrumbs
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
-    Component.Explorer({
-      folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-      folderDefaultState: "collapsed",
-      useSavedState: true,
-    })
-    
+    Component.TagList()
   ],
   left: [
     Component.PageTitle(),
@@ -35,9 +31,41 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 1.1, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
+        fontSize: 0.6, // what size should the node labels be?
+        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        showTags: true, // whether to show tags in the graph
+      },
+      globalGraph: {
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 0.9,
+        repelForce: 0.5,
+        centerForce: 0.3,
+        linkDistance: 30,
+        fontSize: 0.6,
+        opacityScale: 1,
+        removeTags: ["index"], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.Explorer({
+      title: "Notes", // title of the explorer component
+      folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+      folderDefaultState: "collapsed",
+      useSavedState: true,
+    })
   ],
 }
 
